c01b5b9cd2d45e6f2dc34d31af547ebb21e5d365
DS1 <DS1@DESKTOP-F9EAVOG>
2025-07-31 16:24:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Vending Machine NRT";
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            CenterText(@" 
+                        
+            ███▄▄▄▄      ▄████████     ███     
+            ███▀▀▀██▄   ███    ███ ▀█████████▄ 
+            ███   ███   ███    ███    ▀███▀▀██ 
+            ███   ███  ▄███▄▄▄▄██▀     ███   ▀ 
+            ███   ███ ▀▀███▀▀▀▀▀       ███     
+            ███   ███ ▀███████████     ███     
+            ███   ███   ███    ███     ███     
+             ▀█   █▀    ███    ███    ▄████▀   
+                        ███    ███             
+           ");
+              
+            Console.ResetColor();
+            
+            WriteLine("Welcome to NRT Vending Machine! Select an option below:");
+            Console.ReadKey(true);
+
+
+
+            static void CenterText(string text)
+            {
+                int windowWidth = Console.WindowWidth;
+                int padding = Math.Max(0, (windowWidth - text.Length) / 2);
+                Console.WriteLine(new string(' ', padding) + text);
+
+            }
+        }
+    }
+}
